6b679d221e0f9dd317ccd0fe13a3cd3733d4a111
SlasshyOverhere <5boned00@gmail.com>
2026-03-02 09:44:18 +0530

    chore: relax blocking lint rule in initial config

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'no-async-promise-executor': 'off',
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'off',
     },
